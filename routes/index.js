f887a4ee56b61b825d2a541056079aecc9c00d40
Jamie Garner <jamiegarner123@hotmail.co.uk>
2018-03-01 15:41:28 +0000

    test 2

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,8 @@ var queries = require('../queries');
 router.get('/events', queries.getAllEvents);
 
 router.get('*', function(req, res, next){
-  res.status(404).send('Route Not Found. Valid Routes - /events');
+  router.get('/events', queries.getAllEvents);
+  // res.status(404).send('Route Not Found. Valid Routes - /events');
 });
 
 module.exports = router;
