9953c5ded7db0714ed2a059334292e2c4c8c7da3
Jamie Garner <jamiegarner123@hotmail.co.uk>
2018-03-01 15:51:52 +0000

    cleanup

--- queries/index.js
+++ queries/index.js
@@ -5,18 +5,10 @@ var prodDb = process.env.MONGODB_URI;
 var devDb = 'mongodb://localhost:27017/historical-events';
 var dbHost = process.env.NODE_ENV === 'production' ? prodDb : devDb;
 
-// mongoose.connect(dbHost);
-mongoose.connect(dbHost, function (err) {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log('mongoose connection is successful on: ' + db);
-  }
-});
-
+mongoose.connect(dbHost);
 
 function getAllEvents(req, res, next) {
-  Events.find(function (err, events) {
+  Events.find((err, events) => {
     if (err)
       res.send(err);
     res.json(events);
